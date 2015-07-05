7311cca174c2b137cd9f843853338feb5b79452e
sdclibbery <sdclibbery@googlemail.com>
2015-07-05 23:08:46 +0100

    change mind: return events from metronome update

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 requirejs(['play/play', 'music/metronome', 'perform/drummer'], function(play, metronome, drummer) {
   try { if (!AudioContext) { throw 1; } } catch(e) { document.body.innerHTML = 'Web Audio not supported in this browser!'; }
 
-  metronome.listenToBeat(drummer.beat);
-
   var tick = function () {
-    metronome.update(play.timeNow());
+    metronome.update(play.timeNow()).map(drummer.beat);
     requestAnimationFrame(tick);
   };
   requestAnimationFrame(tick);
--- music/metronome.js
+++ music/metronome.js
@@ -4,12 +4,13 @@ define([], function () {
 
 var metronome = {
   bpm: 110,
-  nextBeat: 0,
-  nextBeatIdx: 0,
   beats: [
     { strength: 'down', subBeats: [ 0.5 ] },
     { strength: 'up', subBeats: [ 0.5 ] }
-  ]
+  ],
+
+  nextBeat: 0,
+  nextBeatIdx: 0
 };
 
 metronome.nextBeatAt = function () {
@@ -20,19 +21,17 @@ metronome.beatDuration = function () {
   return 60 / metronome.bpm;
 };
 
-metronome.listenToBeat = function (action) {
-  metronome.action = action;
-};
-
 metronome.update = function (now) {
+  var events = [];
   if (now > metronome.nextBeat - 0.1) { // Call back just BEFORE the next beat to make sure that events composed ON the beat can be scheduled accurately
     var beat = metronome.beats[metronome.nextBeatIdx];
     beat.time = metronome.nextBeatAt();
     beat.duration = metronome.beatDuration();
-    if (metronome.action) { metronome.action(beat); }
+    events.push(beat);
     metronome.nextBeat += metronome.beatDuration();
     metronome.nextBeatIdx = (metronome.nextBeatIdx + 1) % metronome.beats.length;
   }
+  return events;
 };
 
 return metronome;
