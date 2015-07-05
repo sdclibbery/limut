6b9630b979a6c257715b1d4f6992203a42e9bb1b
sdclibbery <sdclibbery@googlemail.com>
2015-07-05 19:30:12 +0100

    use requestanimationframe

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 requirejs(['play/play', 'play/percussion', 'music/metronome'], function(play, percussion, metronome) {
   try { if (!AudioContext) { throw 1; } } catch(e) { document.body.innerHTML = 'Web Audio not supported in this browser!'; }
 
-  setInterval(function () {
-    metronome.update(play.timeNow(), function (beat) {
-      if (beat.strength === 'down') {
-        percussion.kick(beat.time);
-      }
-      if (beat.strength === 'up') {
-        percussion.snare(beat.time);
-      }
-      beat.subBeats.map(function (time) {
-        percussion.hat(beat.time + time*beat.duration);
-      });
+  var drummer = function (beat) {
+    if (beat.strength === 'down') {
+      percussion.kick(beat.time);
+    }
+    if (beat.strength === 'up') {
+      percussion.snare(beat.time);
+    }
+    beat.subBeats.map(function (time) {
+      percussion.hat(beat.time + time*beat.duration);
     });
-  }, 100);
+  };
+
+  var tick = function () {
+    metronome.update(play.timeNow(), drummer);
+    requestAnimationFrame(tick);
+  };
+  requestAnimationFrame(tick);
 });
