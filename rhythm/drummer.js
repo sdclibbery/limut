45be4e20a918c2a6dc88fe84ecc1f98761628f2c
sdclibbery <sdclibbery@googlemail.com>
2015-07-12 20:44:05 +0100

    refactor drummer to be data driven

--- rhythm/drummer.js
+++ rhythm/drummer.js
@@ -5,14 +5,19 @@ var percussion = require('rhythm/percussion.play');
 
 var drummer = {};
 
+//var strengthToInstruments = [ [], [], [], [] ]; // List (indexed by beat strength) of instruments to play for that strength
+var strengthToInstruments = [
+  [],
+  [ 'hat' ],
+  [ 'snare' ],
+  [ 'kick' ]
+];
+
 drummer.event = function (event) {
-  if (event.strength === 3) {
-    percussion.kick(event.time);
-  } else if (event.strength === 2) {
-    percussion.snare(event.time);
-  } else if (event.strength === 1) {
-    percussion.hat(event.time);
-  }
+  var instruments = strengthToInstruments[event.strength];
+  instruments.map(function (instrument) {
+    percussion[instrument](event.time);
+  });
 };
 
 return drummer;
