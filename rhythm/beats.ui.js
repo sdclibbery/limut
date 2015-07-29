8b75eab1ab7598b5ad42a3dfd95c9c1da470f03b
sdclibbery <sdclibbery@googlemail.com>
2015-07-29 21:23:18 +0100

    instantiable DOM callback for beats UI

--- rhythm/beats.ui.js
+++ rhythm/beats.ui.js
@@ -2,6 +2,7 @@
 
 define(function (require) {
 var metronome = require('rhythm/metronome');
+var id = 1;
 
 var make = function (info) {
   var values = info.values;
@@ -9,9 +10,10 @@ var make = function (info) {
   var myBeats = info.beats;
   var beatsUI = { lookup: values };
 
-  var markup = '<textarea id="beats" rows="9" cols="32" oninput="beatsUIChange(this)">'+initial+'</textarea>';
-  window.beatsUIChange = function (ta) {
-    myBeats.values(parse(ta.value));
+  id++;
+  var markup = '<textarea id="beats" rows="9" cols="32" oninput="beatsUIChange'+id+'(this)">'+initial+'</textarea>';
+  window['beatsUIChange'+id] = function (ta) {
+    myBeats.values(parse(ta.value, beatsUI.lookup));
   };
 
   var html = '<div class="widget"><h5>beats</h5>'+markup+'</div>';
