cf0720c74cbbece8100c01b99ab6a05fd4d45619
sdclibbery <sdclibbery@googlemail.com>
2020-06-04 21:42:11 +0100

    ctrl enter to parse

--- main.js
+++ main.js
@@ -8,21 +8,25 @@ define(function(require) {
   };
   let playerInstances = {};
 
-  window.parse = (textarea) => {
-    textarea.value.split('/n').map(l => l.trim()).filter(l => l != '').map(line => {
-      let parts = line.split(/(\s+)/).map(p => p.trim()).filter(p => p != '')
-      let playerId = parts[0].trim()
-      if (playerId) {
-        let playerName = parts[1].trim()
-        if (playerName) {
-          let command  = parts.slice(2).join('').trim()
-          playerInstances[playerId] = players[playerName](command)
-        } else {
-          delete playerInstances[playerId]
+  let textarea = document.getElementById('code')
+  textarea.addEventListener("keydown", event => {
+    if (event.isComposing || event.keyCode === 229) { return; }
+    if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey) {
+      textarea.value.split('\n').map(l => l.trim()).filter(l => l != '').map(line => {
+        let parts = line.split(/(\s+)/).map(p => p.trim()).filter(p => p != '')
+        let playerId = parts[0].trim()
+        if (playerId) {
+          let playerName = parts[1].trim()
+          if (playerName) {
+            let command  = parts.slice(2).join('').trim()
+            playerInstances[playerId] = players[playerName](command)
+          } else {
+            delete playerInstances[playerId]
+          }
         }
-      }
-    })
-  }
+      })
+    }
+  });
 
   let tick = function () {
     let beat = metronome.update(play.timeNow());
