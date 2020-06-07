d82e9635e0e76a079fe13ef9a1dbcbad9202eda4
sdclibbery <sdclibbery@googlemail.com>
2020-06-07 19:59:52 +0100

    support dur

--- player/pattern.js
+++ player/pattern.js
@@ -2,16 +2,20 @@ define(function(require) {
 
   let parsePattern = (pattern, params) => {
     let steps = pattern.split('')
-    let dur = params.dur || 1
-    let stepsPerBeat = 1/dur
-    events = []
+    let dur = eval(params.dur) || 1
+    events = [[]]
+    let time = 0
     steps.forEach((step, idx) => {
       let sound = steps[idx]
-      let time = 0
       event = Object.assign({sound:sound, time:time}, params)
-      events.push([event])
+      events[events.length-1].push(event)
+      time += dur
+      if (time >= 1) {
+        time -= 1
+        events.push([])
+      }
     })
-    return events
+    return events.filter(x => x.length>0)
   }
 
   // TESTS //
@@ -28,13 +32,15 @@ define(function(require) {
 
   assert([[{sound:'x',time:0}],[{sound:'o',time:0}]], parsePattern('xo', {}))
 
-  //assert([[{sound:'x',time:0},{sound:'o',time:1/2}]], parsePattern('xo', {dur:1/2}))
+  assert([[{sound:'x',time:0,dur:1/2},{sound:'o',time:1/2,dur:1/2}]], parsePattern('xo', {dur:1/2}))
 
   //assert([[{sound:'x',time:0},{sound:'o',time:1/2}]], parsePattern('[xo]', {}))
 
   //assert([[{sound:'x',time:0}],[{sound:'.',time:0}],[{sound:'o',time:0}],[{sound:'.',time:0}]], parsePattern('(xo).', {}))
 
   //etc
+
+  console.log("Pattern tests complete")
 
   return parsePattern
 });
