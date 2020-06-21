3369c5a6caa42981bab6aaf46c93f08deda0f5a6
sdclibbery <sdclibbery@googlemail.com>
2020-06-21 19:42:15 +0100

    proper param parsing

--- player/params.js
+++ player/params.js
@@ -1,16 +1,69 @@
 define(function(require) {
 
-  let splitOnAll = (str, ch) => {
-    if (!str) { return [] }
-    return str.split(ch).map(x => x.trim()).filter(x => x!=ch)
+  let parseName = (state) => {
+    let name = ''
+    let char
+    while (char = state.str.charAt(state.idx)) {
+      if (char == '=') {
+        state.idx += 1
+        return name
+      } else {
+        name = name+char
+        state.idx += 1
+      }
+    }
+    return null
+  }
+
+  let brackets = {
+    '[': ']',
+    '(': ')',
+    '{': '}',
+    '<': '>',
+  }
+
+  let parseValue = (state) => {
+    let value = ''
+    let char
+    while (char = state.str.charAt(state.idx)) {
+      if (brackets[char]) {
+        value = value+char
+        state.bracketStack.push(brackets[char])
+        state.idx += 1
+      } else if (char == state.bracketStack[state.bracketStack.length-1]) {
+        value = value+char
+        state.bracketStack.pop()
+        state.idx += 1
+      } else if (char == ',' && state.bracketStack.length == 0) {
+        state.idx += 1
+        return value
+      } else {
+        value = value+char
+        state.idx += 1
+      }
+    }
+    return value
+  }
+
+  let parseParam = (state) => {
+    let name = parseName(state)
+    let value = parseValue(state)
+    if (name) {
+      state.params[name.toLowerCase().trim()] = value.trim()
+      return true
+    }
+    return false
   }
 
   let parseParams = (paramsStr) => {
-    let params = {}
-    splitOnAll(paramsStr, ', ')
-      .map(p => splitOnAll(p, '='))
-      .forEach(([n,v]) => params[n.toLowerCase()] = v)
-    return params
+    let state = {
+      str: paramsStr,
+      idx: 0,
+      params: {},
+      bracketStack: [],
+    }
+    while (parseParam(state)) {}
+    return state.params
   }
 
   // TESTS //
@@ -23,11 +76,17 @@ define(function(require) {
 
   assert({}, parseParams(''))
   assert({dur:'1'}, parseParams('dur=1'))
+  assert({dur:'1'}, parseParams('Dur=1'))
   assert({dur:'1', oct:'4'}, parseParams('dur=1, oct=4'))
   assert({dur:'4', oct:'5', decay:'2', attack:'2'}, parseParams('dur=4, oct=5, decay=2, attack=2'))
   assert({dur:'1/2'}, parseParams('dur=1/2'))
   assert({dur:'[1]'}, parseParams('dur=[1]'))
   assert({dur:'[1,1]'}, parseParams('dur=[1,1]'))
+  assert({dur:'1', oct:'4'}, parseParams('dur=1,oct=4'))
+  assert({dur:'[1, 1]'}, parseParams('dur=[1, 1]'))
+  assert({dur:'[1, 2]', oct:'[3, 4]'}, parseParams('dur=[1, 2],oct=[3, 4]'))
+  assert({dur:'[[1,1],[[2],3]]', oct:'4'}, parseParams('dur=[[1,1],[[2],3]],oct=4'))
+  assert({dur:'([1,1],<{2},3>)', oct:'4'}, parseParams('dur=([1,1],<{2},3>),oct=4'))
 
   console.log("Params tests complete")
 
