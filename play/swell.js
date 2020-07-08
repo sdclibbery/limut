1d8a087ab6e4bb05268f2d46aca30c3f2cfa295e
sdclibbery <sdclibbery@googlemail.com>
2020-07-08 08:27:48 +0100

    quieter swell

--- play/swell.js
+++ play/swell.js
@@ -12,7 +12,7 @@ define(function (require) {
     let freq = scale.degreeToFreq(degree, param(params.oct, 4))
     let detuneSemis = param(params.detune, 0.1)
 
-    let vca = envelope(params, 0.04)
+    let vca = envelope(params, 0.02)
     system.mix(effects(params, vca))
 
     let vcos = [0, 0.2].map(lerp => {
