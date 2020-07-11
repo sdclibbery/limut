21f6429482c2bda41192f8ea6590047c5b5a18d8
sdclibbery <sdclibbery@googlemail.com>
2020-07-11 20:15:51 +0100

    tweak bell

--- play/bell.js
+++ play/bell.js
@@ -33,27 +33,27 @@ define(function (require) {
   return (params) => {
     let degree = parseInt(params.sound) + param(params.add, 0)
     if (isNaN(degree)) { return }
-    let freq = scale.degreeToFreq(degree, param(params.oct, 4))
-//    let detuneSemis = param(params.detune, 0.1)
+    let freq = scale.degreeToFreq(degree, param(params.oct, 4))/4
+    let detune = param(params.detune, 0.1)
 
     let vca = envelope(params, 0.01)
     system.mix(effects(params, vca))
 
-    let vco4 = fmOp(freq*21.98/3.14, 0, params)
+    let vco4 = fmOp(freq*21.98, 0, params)
     vco4.connect(vca)
 
-    let vco3 = fmOp(freq*10.38/3.14, 1, params)
+    let vco3 = fmOp(freq*10.38, 1*detune*10, params)
     let vco3_4gain = flatEnv(700)
     vco3.connect(vco3_4gain)
     vco3_4gain.connect(vco4.detune)
 
-    let vco2 = fmOp(freq, 3, params)
-    let vco2_4gain = simpleEnv(150, params, 0, 0.7)
+    let vco2 = fmOp(freq*3.14, 3*detune*10, params)
+    let vco2_4gain = simpleEnv(150, params, 3, 0.7)
     vco2.connect(vco2_4gain)
     vco2_4gain.connect(vco4.detune)
 
-    let vco1 = fmOp(freq*19.03/3.14, 1, params)
-    let vco1_4gain = simpleEnv(10000, params, 0, 0.3)
+    let vco1 = fmOp(freq*19.03, 1*detune*10, params)
+    let vco1_4gain = simpleEnv(10000, params, 1, 0.3)
     vco1.connect(vco1_4gain)
     vco1_4gain.connect(vco1.detune)
     vco1_4gain.connect(vco4.detune)
