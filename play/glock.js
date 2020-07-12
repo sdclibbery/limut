cf31a1c40e84b4fc2b61d35e5c07588fe923939f
sdclibbery <sdclibbery@googlemail.com>
2020-07-12 09:23:05 +0100

    glock synth

---
+++ play/glock.js
@@ -0,0 +1,33 @@
+'use strict';
+define(function (require) {
+  let system = require('play/system');
+  let fm = require('play/fm')
+  let scale = require('music/scale');
+  let envelope = require('play/no-sus-envelope')
+  let effects = require('play/effects')
+  let param = require('player/default-param')
+
+  return (params) => {
+    params.sus = param(params.sus, 2)
+    let degree = parseInt(params.sound) + param(params.add, 0)
+    if (isNaN(degree)) { return }
+    let freq = scale.degreeToFreq(degree, param(params.oct, 4))
+
+    let vca = envelope(params, 0.01)
+    system.mix(effects(params, vca))
+
+    let op4 = fm.op(freq, params)
+    op4.connect(vca)
+
+    let op3 = fm.op(freq*14, params)
+    op3.connect(vca)
+
+    let op2 = fm.op(freq, params)
+    op2.connect(vca)
+
+    let op1 = fm.op(freq*5, params, 'square')
+    fm.connect(op1, op2, fm.simpleEnv(param(window.glock1, 3000), params, 0, 0.5))
+
+    // frequencies from element433 https://youtu.be/CdPa6VLi4GQ?t=191
+  }
+});
