41b118bf1cb3bcf6beedb5dabdc596830c55e1b9
sdclibbery <sdclibbery@googlemail.com>
2020-07-12 08:55:05 +0100

    better bell?

--- play/bell.js
+++ play/bell.js
@@ -21,41 +21,37 @@ define(function (require) {
     vca.gain.value = gain
     return vca
   }
-  let fmOp = (freq, detune,  params) => {
+  let fmOp = (freq, params, wave) => {
     let vco = system.audio.createOscillator()
-    vco.type = 'sine';
+    vco.type = wave || 'sine';
     vco.frequency.value = freq
-    vco.detune.value = detune
     vco.start(params.time)
     vco.stop(params.endTime)
     return vco
   }
+  let fmConnect = (modulator, carrier, envelope) => {
+    modulator.connect(envelope)
+    envelope.connect(carrier.frequency)
+  }
   return (params) => {
     let degree = parseInt(params.sound) + param(params.add, 0)
     if (isNaN(degree)) { return }
-    let freq = scale.degreeToFreq(degree, param(params.oct, 4))/4
-    let detune = param(params.detune, 0.1)
+    let freq = scale.degreeToFreq(degree, param(params.oct, 4))
 
-    let vca = envelope(params, 0.01)
+    let vca = envelope(params, 0.05)
     system.mix(effects(params, vca))
 
-    let vco4 = fmOp(freq*21.98, 0, params)
-    vco4.connect(vca)
+    let op4 = fmOp(freq*21.98/3.14, params)
+    op4.connect(vca)
 
-    let vco3 = fmOp(freq*10.38, 1*detune*10, params)
-    let vco3_4gain = flatEnv(700)
-    vco3.connect(vco3_4gain)
-    vco3_4gain.connect(vco4.detune)
+    let op3 = fmOp(freq*10.38/3.14, params)
+    fmConnect(op3, op4, flatEnv(param(window.bell3, 702)))
 
-    let vco2 = fmOp(freq*3.14, 3*detune*10, params)
-    let vco2_4gain = simpleEnv(150, params, 3, 0.7)
-    vco2.connect(vco2_4gain)
-    vco2_4gain.connect(vco4.detune)
+    let op2 = fmOp(freq*3.14/3.14, params)
+    fmConnect(op2, op4, envelope(params, param(window.bell2, 386)))
 
-    let vco1 = fmOp(freq*19.03, 1*detune*10, params)
-    let vco1_4gain = simpleEnv(10000, params, 1, 0.3)
-    vco1.connect(vco1_4gain)
-    vco1_4gain.connect(vco1.detune)
-    vco1_4gain.connect(vco4.detune)
+    let op1 = fmOp(freq*19.03/3.14, params, 'sawtooth')
+    fmConnect(op1, op2, simpleEnv(param(window.bell1, 300), params, 0, 1))
+    //    fmConnect(op1, op1, flatEnv(param(window.bell1fb, 2))) // feedback doesnt work?
   }
 });
