b706993e9b767048e38f7dfadcaf79f7e0e5e34b
sdclibbery <sdclibbery@googlemail.com>
2020-08-02 12:48:29 +0100

    better default colours

--- parse-line.js
+++ parse-line.js
@@ -35,9 +35,9 @@ define((require) => {
     // visualisations
     clouds: makePlayerFactory(sprite('clouds', {r:1,g:1,b:1,a:1}, {r:0.15,g:0.2,b:1,a:1})),
     kal: makePlayerFactory(sprite('kaleidoscope', {r:1,g:0.7,b:0.3,a:1}, {r:0,g:0.05,b:0.2,a:1})),
-    swirl: makePlayerFactory(sprite('swirl', {r:1,g:0.5,b:0,a:1}, {r:0,g:0.0,b:0.4,a:1})),
-    lines: makePlayerFactory(sprite('lines', {r:1,g:0.5,b:0.0,a:1}, {r:0,g:0.0,b:0.4,a:1})),
-    blob: makePlayerFactory(sprite('blob', {r:0,g:0.3,b:0.9,a:1}, {r:0,g:0,b:0,a:0})),
+    swirl: makePlayerFactory(sprite('swirl', {r:0.9,g:0.9,b:0.9,a:1}, {r:0.1,g:0.1,b:0.3,a:1})),
+    lines: makePlayerFactory(sprite('lines', {r:0.9,g:0.9,b:0.9,a:1}, {r:0,g:0,b:0,a:0})),
+    blob: makePlayerFactory(sprite('blob', {r:0.8,g:0.8,b:0.8,a:1}, {r:0,g:0,b:0,a:0})),
     // instruments
     drums: makePlayerFactory(percussion.play, 1/2),
     play: makePlayerFactory(play, 1/2),
