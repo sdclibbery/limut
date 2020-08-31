8bc0744f8e265502ecb84a4cb0d9640a16c69455
sdclibbery <sdclibbery@googlemail.com>
2020-08-31 08:52:21 +0100

    pad envelope has crossfade curves so it can sound continuous

--- play/pad-envelope.js
+++ play/pad-envelope.js
@@ -3,6 +3,13 @@ define(function (require) {
   let system = require('play/system');
   let param = require('player/default-param')
 
+  let fade = (from, to) => {
+    return (new Float32Array(16)).map((_,i) => {
+      let lerp = Math.sin(i/15 * 0.5*Math.PI)
+      return from*(1-lerp) + to*lerp
+    })
+  }
+
   return (params, gainBase) => {
     let dur = Math.max(0.01, param(params.sus, param(params.dur, 0.25)))
     let attack = param(params.att, dur/2) * params.beat.duration
@@ -12,9 +19,9 @@ define(function (require) {
     let vca = system.audio.createGain()
     vca.gain.cancelScheduledValues(params.time)
     vca.gain.setValueAtTime(0, params.time)
-    vca.gain.linearRampToValueAtTime(gain, params.time + attack)
+    vca.gain.setValueCurveAtTime(fade(0, gain), params.time, attack)
     vca.gain.linearRampToValueAtTime(gain, params.time + attack+sus)
-    vca.gain.linearRampToValueAtTime(0, params.time + attack+sus+release)
+    vca.gain.setValueCurveAtTime(fade(gain, 0), params.time + attack+sus, release)
     params.endTime = params.time + attack+sus+release
     return vca
   }
