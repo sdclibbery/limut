06d15ca871eb1770c0f6cb6dbd9eeee9fcfdcf30
sdclibbery <sdclibbery@googlemail.com>
2020-09-19 08:51:02 +0100

    ready to eval per frame or event

--- player/eval-param.js
+++ player/eval-param.js
@@ -1,24 +1,33 @@
 'use strict';
 define((require) => {
 
-  let evalParam = (value, step, beat) => {
+  let evalParamNow = (value, step, beat) => {
     if (Array.isArray(value)) {
       let v = value[step % value.length]
-      if (typeof v == 'function') { return evalParam(v, step, beat) }
+      if (typeof v == 'function') { return evalParamNow(v, step, beat) }
       return v
     } else if (typeof value == 'function') {
       let v = value(step, beat)
       if (Array.isArray(v)) { return v }
-      return evalParam(v, step, beat)
+      return evalParamNow(v, step, beat)
     } else if (typeof value == 'object') {
       let result = {}
       for (let k in value) {
-        result[k] = evalParam(value[k], step, beat)
+        result[k] = evalParamNow(value[k], step, beat)
       }
       return result
     } else {
       return value
     }
+  }
+
+  let evalParamFrame = (value, step, beat) => {
+    return evalParamNow(value, step, beat)
+  }
+
+  let evalParamEvent = (value, step, beat) => {
+    if (value.interval === 'frame') { return value }
+    return evalParamNow(value, step, beat)
   }
 
   // TESTS //
@@ -29,23 +38,34 @@ define((require) => {
     if (x !== a) { console.trace(`Assertion failed.\n>>Expected:\n  ${x}\n>>Actual:\n  ${a}`) }
   }
 
-  assert(1, evalParam(1, 0, 0))
-  assert(1/2, evalParam(1/2, 0, 0))
-  assert(1, evalParam([1,2,3], 0, 0))
-  assert(2, evalParam([1,2,3], 1, 0))
-  assert(3, evalParam([1,2,3], 2, 0))
-  assert(1, evalParam([1,2,3], 3, 0))
-  assert(5, evalParam(() => 5, 0, 0))
-  assert(5, evalParam((x,y) => y, 0, 5))
-  assert([1,2], evalParam([()=>[1,2]], 0, 0))
-  assert([3,4], evalParam([()=>[1,2],()=>[3,4]], 1, 0))
-  assert({x:1}, evalParam({x:[1,2]}, 0, 0))
-  assert({x:2}, evalParam({x:[1,2]}, 1, 1))
-  assert('a', evalParam('a', 0, 0))
-  assert('a', evalParam(['a','b'], 0, 0))
-  assert('b', evalParam(['a','b'], 1, 1))
+  assert(1, evalParamEvent(1, 0, 0))
+  assert(1/2, evalParamEvent(1/2, 0, 0))
+  assert(1, evalParamEvent([1,2,3], 0, 0))
+  assert(2, evalParamEvent([1,2,3], 1, 0))
+  assert(3, evalParamEvent([1,2,3], 2, 0))
+  assert(1, evalParamEvent([1,2,3], 3, 0))
+  assert(5, evalParamEvent(() => 5, 0, 0))
+  assert(5, evalParamEvent((x,y) => y, 0, 5))
+  assert([1,2], evalParamEvent([()=>[1,2]], 0, 0))
+  assert([3,4], evalParamEvent([()=>[1,2],()=>[3,4]], 1, 0))
+  assert({x:1}, evalParamEvent({x:[1,2]}, 0, 0))
+  assert({x:2}, evalParamEvent({x:[1,2]}, 1, 1))
+  assert('a', evalParamEvent('a', 0, 0))
+  assert('a', evalParamEvent(['a','b'], 0, 0))
+  assert('b', evalParamEvent(['a','b'], 1, 1))
+
+  let perFrameValue = () => 3
+  perFrameValue.interval= 'frame'
+  assert(1, evalParamFrame(1, 0, 0))
+  assert(perFrameValue, evalParamEvent(perFrameValue, 0, 0))
+  assert(3, evalParamFrame(perFrameValue, 0, 0))
 
   console.log('Eval param tests complete')
 
-  return evalParam
+  return evalParamFrame
+  // return {
+  //   evalParamEvent:evalParamEvent,
+  //   evalParamFrame:evalParamFrame,
+  // }
+
 })
