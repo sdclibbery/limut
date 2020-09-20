bd60a4b1226cfc490441680a36a81aba6bfae401
sdclibbery <sdclibbery@googlemail.com>
2020-09-20 23:52:11 +0100

    fix amp failing when per frame

--- player/parse-player.js
+++ player/parse-player.js
@@ -38,7 +38,7 @@ define((require) => {
         if (!playerFactory) { throw 'Player "'+playerType+'" not found' }
         let play = (es) => {
           return es
-            .filter(e => e.amp === undefined || e.amp > 0)
+            .filter(e => e.amp === undefined || e.amp > 0 || typeof e.amp === 'function')
             .map(e => {
               playerFactory.play(e)
               let pulse = (s,b) => {
