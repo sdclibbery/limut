e9ea0ff12fcfb7a403ff156d36611e22e7d3bc2d
sdclibbery <sdclibbery@googlemail.com>
2020-09-20 19:40:59 +0100

    pitch effects eval params

--- play/pitch-effects.js
+++ play/pitch-effects.js
@@ -1,13 +1,13 @@
 'use strict';
 define(function (require) {
   let system = require('play/system')
-  let param = require('player/default-param')
+  let {evalPerEvent,evalPerFrame} = require('play/eval-audio-params')
 
   return (params) => {
-    let vib = param(params.vib, 0)
+    let vib = evalPerEvent(params, 'vib', 0)
     if (!vib) { return {connect:()=>{}} }
-    let vibdepth = param(params.vibdepth, 0.4)
-    let vibdelay = param(params.vibdelay, 1)
+    let vibdepth = evalPerEvent(params, 'vibdepth', 0.4)
+    let vibdelay = evalPerEvent(params, 'vibdelay', 1)
     let lfo = system.audio.createOscillator()
     lfo.type = 'sine'
     lfo.frequency.value = vib / params.beat.duration
