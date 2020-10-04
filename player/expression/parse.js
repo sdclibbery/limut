cd92c6c93d0abb1a414953bc5fd040e4a8d2ed53
sdclibbery <sdclibbery@googlemail.com>
2020-10-04 20:49:05 +0100

    parse single operators

--- player/expression/parse.js
+++ player/expression/parse.js
@@ -2,9 +2,41 @@
 define(function(require) {
   let number = require('player/parse-number')
 
+  let eatWhitespace = (state) => {
+    let char
+    while (char = state.str.charAt(state.idx)) {
+      if (char === ' ' || char === '\t' || char === '\n' || char === '\r') { state.idx += 1; continue }
+      break
+    }
+  }
+
+  let parseEval = (state) => {
+    eatWhitespace(state)
+    let result
+    if (state.str.charAt(state.idx) == '@') {
+      state.idx += 1
+      if (state.str.charAt(state.idx) == 'f') {
+        state.idx += 1
+        result = 'frame'
+      } else if (state.str.charAt(state.idx) == 'e') {
+        state.idx += 1
+        result = 'event'
+      }
+    }
+    return result
+  }
+
+  let parseOperators = (ops) => {
+    return {
+      type: 'operator'+ops[1],
+      lhs:ops[0],
+      rhs:ops[2],
+    }
+  }
+
   let expression = (state) => {
-    // console.log('expression', state)
-    let result = {eval:'constant', type:'undefined'}
+    let expr = {eval:'constant', type:'undefined'}
+    let operatorList = []
     let char
     while (char = state.str.charAt(state.idx)) {
       if (char === '') { break }
@@ -15,21 +47,35 @@ define(function(require) {
         state.commented = true
         break
       }
+      // operator
+      if (expr.type !== 'undefined') {
+        if (['+','-','*','/'].includes(char)) {
+          state.idx += 1
+          eatWhitespace(state)
+          operatorList.push(expr)
+          expr = {eval:'constant', type:'undefined'}
+          operatorList.push(char)
+          operatorList.push(expression(state))
+          continue
+        }
+      }
       // number
       let n = number(state)
       if (n !== undefined) {
-        result = {value: n, eval: 'constant', type: 'number' }
-        // console.log('number', result, state)
+        expr = {value: n, eval: 'constant', type: 'number' }
         continue
       }
       break
     }
-    return result
+    if (operatorList.length > 0) {
+      expr = parseOperators(operatorList)
+    }
+    expr.eval = parseEval(state) || expr.eval
+    return expr
   }
   
   let parseExpression = (v, commented) => {
     if (v == '' || v == undefined) { return {eval:'constant',type:'undefined'} }
-    // console.log('*** parseExpression', v)
     v = v.trim()
     let state = {
       str: v,
@@ -53,15 +99,26 @@ define(function(require) {
     assert(true, commented)
   }
 
+  let num = (n) => {return {value:n, eval:'constant',type:'number'}}
+
   assert({eval:'constant',type:'undefined'}, parseExpression())
   assert({eval:'constant',type:'undefined'}, parseExpression(''))
   assertCommented({eval:'constant',type:'undefined'}, '//')
 
-  assert({value:1, eval:'constant',type:'number'}, parseExpression('1'))
-  assert({value:-1.1, eval:'constant',type:'number'}, parseExpression('-1.1'))
-  assert({value:0.5, eval:'constant',type:'number'}, parseExpression('1/2'))
+  assert(num(1), parseExpression('1'))
+  assert(num(-1.1), parseExpression('-1.1'))
+  assert(num(0.5), parseExpression('1/2'))
+  assert({value:1, eval:'event',type:'number'}, parseExpression('1@e'))
+  assert({value:1, eval:'frame',type:'number'}, parseExpression('1@f'))
   assertCommented({eval:'constant',type:'undefined'}, '//1')
-  assertCommented({value:1, eval:'constant',type:'number'}, '1//')
+  assertCommented(num(1), '1//')
+
+  assert({type:'operator+',lhs:num(1),rhs:num(2)}, parseExpression('1+2'))
+  assert({type:'operator+',lhs:num(1),rhs:num(2)}, parseExpression(' 1 + 2 '))
+  assert({type:'operator+',lhs:num(-0.5),rhs:num(1.5)}, parseExpression('-1/2+1.5'))
+  assert({type:'operator*',lhs:num(1),rhs:num(2)}, parseExpression('1*2'))
+  assert({type:'operator/',lhs:num(1),rhs:num(2)}, parseExpression('1 / 2'))
+  assert({type:'operator-',lhs:num(1),rhs:num(2)}, parseExpression('1-2'))
 
   console.log('Parse expression tests complete')
 
