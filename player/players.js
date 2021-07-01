cd1a78e87c0281a9036d8632221bc919fdc8b7e1
sdclibbery <sdclibbery@googlemail.com>
2021-07-01 21:57:58 +0100

    fix bug where old players didnt get cleaned up properly

--- player/players.js
+++ player/players.js
@@ -1,29 +1,25 @@
 'use strict'
 define(function(require) {
-  let instances = {}
-  let overrides = {}
+  let players = {
+    instances: {},
+    overrides: {},
+  }
 
-  let gc_reset = () => {
-    for (let id in instances) {
-      instances[id].marked = false
+  players.gc_reset = () => {
+    for (let id in players.instances) {
+      players.instances[id].marked = false
     }
   }
-  let gc_mark = (id) => {
-    instances[id].marked = true
+  players.gc_mark = (id) => {
+    players.instances[id].marked = true
   }
-  let gc_sweep = () => {
-    for (let id in instances) {
-      if (!instances[id].marked) {
-        delete instances[id]
+  players.gc_sweep = () => {
+    for (let id in players.instances) {
+      if (!players.instances[id].marked) {
+        delete players.instances[id]
       }
     }
   }
 
-  return {
-    instances: instances,
-    overrides: overrides,
-    gc_reset: gc_reset,
-    gc_mark: gc_mark,
-    gc_sweep: gc_sweep,
-  }
+  return players
 })
