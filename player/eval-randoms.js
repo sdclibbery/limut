7b15ccd828e576448064a6d395b00fefeb243453
Stephen Clibbery <sdclibbery@laptop.local>
2021-09-03 08:20:24 +0100

    seedable prng

--- player/eval-randoms.js
+++ player/eval-randoms.js
@@ -3,6 +3,15 @@ define(function(require) {
   let evalOperator = require('player/eval-operator')
   let param = require('player/default-param')
 
+  let mulberry32 = (a) => {
+    return () => {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+  }
+
   let add = (a,b) => a+b
   let mul = (a,b) => a*b
   let lerpValue = (lerp, pre, post) => {
@@ -78,6 +87,9 @@ define(function(require) {
   let parseRandom = (vs, period, config, interval) => {
     let evaluator
     let generator = Math.random
+    if (config && config.seed !== undefined) {
+      generator = mulberry32(config.seed)
+    }
     if (vs.length == 0) {
       evaluator = (e,b,evalRecurse) => evalRandomRanged(generator, 0, 1)
     } else if (vs.separator == ':') {
@@ -98,9 +110,10 @@ define(function(require) {
   if ((new URLSearchParams(window.location.search)).get('test') !== null) {
 
     let assert = (expected, actual) => {
-      let x = JSON.stringify(expected, (k,v) => (typeof v == 'number') ? (v+0.0001).toFixed(2) : v)
-      let a = JSON.stringify(actual, (k,v) => (typeof v == 'number') ? (v+0.0001).toFixed(2) : v)
-      if (x !== a) { console.trace(`Assertion failed.\n>>Expected:\n  ${x}\n>>Actual:\n  ${a}`) }
+      if (expected !== actual) { console.trace(`Assertion failed.\n>>Expected equal:\n  ${expected}\n>>Actual:\n  ${actual}`) }
+    }
+    let assertNotEqual = (expected, actual) => {
+      if (expected === actual) { console.trace(`Assertion failed.\n>>Expected unequal:\n  ${expected}\n>>Actual:\n  ${actual}`) }
     }
     let assertIs1OfEveryTime = (expected, getter) => {
       for (let i=0; i<20; i++) {
@@ -135,16 +148,22 @@ define(function(require) {
     p = parseRandom(vs)
     assertIsInRangeEveryTime(3,5, () => evalParam(p,ev(0),0))
 
+    // Always gives same value at same time when periodic, even when not seeded
     p = parseRandom([3,5], 1)
     assertIsSameEveryTime(() => evalParam(p,ev(0),0))
     assertIsSameEveryTime(() => evalParam(p,ev(1),1))
 
-    // p = parseRandom([], undefined, {seed:1})
-    // assert(0, evalParam(p,ev(0),0))
-    // assert(0, evalParam(p,ev(0),0))
-    // assert(0, evalParam(p,ev(0),0))
-    // assert(0, evalParam(p,ev(0),0))
-    // assert(0, evalParam(p,ev(0),0))
+    // Non fixed value when not seeded
+    p = parseRandom([], undefined, {})
+    assertNotEqual(0.6270739405881613, evalParam(p,ev(0),0))
+
+    // Same sequence every time when seeded
+    p = parseRandom([], undefined, {seed:1})
+    assert(0.6270739405881613, evalParam(p,ev(0),0))
+    assert(0.002735721180215478, evalParam(p,ev(0),0))
+    assert(0.5274470399599522, evalParam(p,ev(0),0))
+    assert(0.9810509674716741, evalParam(p,ev(0),0))
+    assert(0.9683778982143849, evalParam(p,ev(0),0))
 
     console.log('Eval random tests complete')
   }
