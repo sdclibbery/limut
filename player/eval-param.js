7d8bdf3ef06dff67b900bd548189de25425d2afc
Stephen Clibbery <sdclibbery@laptop.local>
2021-12-05 18:45:32 +0000

    split per-event evaluation methods

--- player/eval-param.js
+++ player/eval-param.js
@@ -42,15 +42,19 @@ define((require) => {
     }
   }
 
-  let evalParamFrame = (value, event, beat) => { // Fully evaluate down to a primitive number/string etc
+  let evalParamFrame = (value, event, beat) => { // Fully evaluate down to a primitive number/string etc, allowing the value to change every frame if it wants to
     return evalParamNow(evalParamFrame, value, event, beat)
   }
 
-  let evalParamEvent = (value, event, beat) => { // Evaluate only values that are constant for the entire event
+  let evalParamEvent = (value, event) => { // Fully evaluate down to a primitive number/string etc, fixing the value for the life of the event it is part of
+    return evalParamNow(evalParamEvent, value, event, event.count)
+  }
+
+  let preEvalParam = (value, event) => { // Evaluate only to values that are constant for the entire event
     if (!!value && value.interval === 'frame') {
       return value
     }
-    return evalParamNow(evalParamEvent, value, event, beat)
+    return evalParamNow(preEvalParam, value, event, event.count)
   }
 
   // TESTS //
@@ -61,23 +65,23 @@ define((require) => {
     let a = JSON.stringify(actual, (k,v) => (typeof v == 'number') ? (v+0.0001).toFixed(2) : v)
     if (x !== a) { console.trace(`Assertion failed.\n>>Expected:\n  ${x}\n>>Actual:\n  ${a}`) }
   }
-  let ev = (i,c) => {return{idx:i,count:c}}
+  let ev = (n) => {return{idx:n,count:n}}
 
-  assert(undefined, evalParamEvent(undefined, ev(0), 0))
-  assert(1, evalParamEvent(1, ev(0), 0))
-  assert(1/2, evalParamEvent(1/2, ev(0), 0))
-  assert(5, evalParamEvent(() => 5, ev(0), 0))
-  assert(5, evalParamEvent((x,y) => y, ev(0), 5))
-  assert({x:1}, evalParamEvent({x:()=>1}, ev(0), 0))
-  assert('a', evalParamEvent('a', ev(0), 0))
-  assert([1,2], evalParamEvent([1,2], ev(0), 0))
-  assert([1,5], evalParamEvent([1,() => 5], ev(0), 0))
-  assert([{x:1},{x:2}], evalParamEvent({x:[1,2]}, ev(0), 0))
-  assert([{x:1,y:3},{x:2,y:4}], evalParamEvent({x:[1,2],y:[3,4]}, ev(0), 0))
-  assert([{x:1,y:4},{x:2,y:5},{x:3,y:4}], evalParamEvent({x:[1,2,3],y:[4,5]}, ev(0), 0))
-  assert([1,2,3], evalParamEvent([1,[2,3]], ev(0), 0))
-  assert([{x:1},{x:2},{x:3}], evalParamEvent([{x:1},{x:[2,3]}], ev(0), 0))
-  assert([1,2,3], evalParamEvent([1,() => [2,3]], ev(0), 0))
+  assert(undefined, evalParamEvent(undefined, ev(0)))
+  assert(1, evalParamEvent(1, ev(0)))
+  assert(1/2, evalParamEvent(1/2, ev(0)))
+  assert(5, evalParamEvent(() => 5, ev(0)))
+  assert(5, evalParamEvent((e,b) => b, ev(5)))
+  assert({x:1}, evalParamEvent({x:()=>1}, ev(0)))
+  assert('a', evalParamEvent('a', ev(0)))
+  assert([1,2], evalParamEvent([1,2], ev(0)))
+  assert([1,5], evalParamEvent([1,() => 5], ev(0)))
+  assert([{x:1},{x:2}], evalParamEvent({x:[1,2]}, ev(0)))
+  assert([{x:1,y:3},{x:2,y:4}], evalParamEvent({x:[1,2],y:[3,4]}, ev(0)))
+  assert([{x:1,y:4},{x:2,y:5},{x:3,y:4}], evalParamEvent({x:[1,2,3],y:[4,5]}, ev(0)))
+  assert([1,2,3], evalParamEvent([1,[2,3]], ev(0)))
+  assert([{x:1},{x:2},{x:3}], evalParamEvent([{x:1},{x:[2,3]}], ev(0)))
+  assert([1,2,3], evalParamEvent([1,() => [2,3]], ev(0)))
 
   let perFrameValue = () => 3
   perFrameValue.interval= 'frame'
@@ -85,16 +89,20 @@ define((require) => {
   perEventValue.interval= 'event'
 
   assert(1, evalParamFrame(1, ev(0), 0))
-  assert(perFrameValue, evalParamEvent(perFrameValue, ev(0), 0))
+  assert(3, evalParamEvent(perFrameValue, ev(0)))
+  assert(perFrameValue, preEvalParam(perFrameValue, ev(0)))
   assert(3, evalParamFrame(perFrameValue, ev(0), 0))
   assert(1, evalParamFrame(1, ev(0), 0))
-  assert(4, evalParamEvent(perEventValue, ev(0), 0))
+  assert(4, evalParamEvent(perEventValue, ev(0)))
+  assert(4, preEvalParam(perEventValue, ev(0)))
   assert(4, evalParamFrame(perEventValue, ev(0), 0))
 
   assert({a:4}, evalParamFrame({a:perEventValue}, ev(0), 0))
   assert({a:3}, evalParamFrame({a:perFrameValue}, ev(0), 0))
-  assert({a:4}, evalParamEvent({a:perEventValue}, ev(0), 0))
-  assert('frame', evalParamEvent({a:perFrameValue}, ev(0), 0).a.interval)
+  assert({a:4}, evalParamEvent({a:perEventValue}, ev(0)))
+  assert({a:3}, evalParamEvent({a:perFrameValue}, ev(0)))
+  assert({a:4}, preEvalParam({a:perEventValue}, ev(0)))
+  assert('frame', preEvalParam({a:perFrameValue}, ev(0)).a.interval)
   assert({r:1}, evalParamFrame(()=>{return({r:1})}, ev(0), 0))
   assert([{r:1,g:3},{r:2,g:3}], evalParamFrame(()=>{return({r:()=>[1,2],g:3})}, ev(0), 0))
 
@@ -102,7 +110,7 @@ define((require) => {
   }
 
   return {
-    preEvalParam:evalParamEvent,
+    preEvalParam:preEvalParam,
     evalParamEvent:evalParamEvent,
     evalParamFrame:evalParamFrame,
   }
