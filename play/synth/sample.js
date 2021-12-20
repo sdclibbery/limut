4f68fecb6d3305e1123f8896ccf2a97df993f6c5
Stephen Clibbery <sdclibbery@laptop.local>
2021-12-20 08:38:42 +0000

    samplepitch is subparam

--- play/synth/sample.js
+++ play/synth/sample.js
@@ -3,7 +3,7 @@ define(function (require) {
   let system = require('play/system');
   let {getBuffer} = require('play/samples')
   let effects = require('play/effects/effects')
-  let {evalMainParamEvent} = require('play/eval-audio-params')
+  let {evalMainParamEvent,evalSubParamEvent} = require('play/eval-audio-params')
   let scale = require('music/scale');
   let waveEffects = require('play/effects/wave-effects')
   let envelope = require('play/envelopes')
@@ -13,7 +13,7 @@ define(function (require) {
     if (isNaN(freq)) { return }
     let source = system.audio.createBufferSource()
     source.buffer = getBuffer(evalMainParamEvent(params, 'sample', 'sample/salamander/C4v8.mp3'))
-    let samplePitch = evalMainParamEvent(params, 'samplepitch', 261.6256)
+    let samplePitch = evalSubParamEvent(params, 'sample', 'pitch', 261.6256)
     source.playbackRate.value = freq / samplePitch
     params.endTime = params._time + evalMainParamEvent(params, 'dur', 0.1)*params.beat.duration
 
