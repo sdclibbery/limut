377d3cd803de08391819781e272849a403447495
Stephen Clibbery <sdclibbery@laptop.local>
2022-05-22 23:06:22 +0100

    ceil and round functions

--- maths.js
+++ maths.js
@@ -3,12 +3,18 @@ define(function(require) {
   let vars = require('vars')
   let {mainParam,subParam} = require('player/sub-param')
 
-  let floor = (args) => {
-    let to = subParam(args, 'to', 1)
-    return Math.floor(mainParam(args.value, 0)/to)*to
+  let createFunc = (name, fn) => {
+    let func = (args) => {
+      let to = subParam(args, 'to', 1)
+      return fn(mainParam(args.value, 0)/to)*to
+    }
+    func.isVarFunction = true
+    vars[name] = func
   }
-  floor.isVarFunction = true
-  vars['floor'] = floor
+
+  createFunc('floor', Math.floor)
+  createFunc('ceil', Math.ceil)
+  createFunc('round', Math.round)
 
   // TESTS //
   if ((new URLSearchParams(window.location.search)).get('test') !== null) {
@@ -24,13 +30,25 @@ define(function(require) {
 
   assert(1, evalParamFrame(parseExpression('floor{1.5}'), ev(0,0), 0))
   assert(-2, evalParamFrame(parseExpression('floor{-1.5}'), ev(0,0), 0))
+  assert(1/2, evalParamFrame(parseExpression('floor{0.6,to:1/2}'), ev(0,0), 0))
+  assert(-1.5, evalParamFrame(parseExpression('floor{-1.2,to:1/2}'), ev(0,0), 0))
+
   assert(1, evalParamFrame(parseExpression('floor{[1.5,2.5]t1@e}'), ev(0,0), 0))
   assert(2, evalParamFrame(parseExpression('floor{[1.5,2.5]t1@e}'), ev(1,1), 1))
   assert(1, evalParamFrame(parseExpression('floor{[1.5,2.5]t1@f}'), ev(0,0), 0))
   assert(2, evalParamFrame(parseExpression('floor{[1.5,2.5]t1@f}'), ev(1,1), 1))
   assert([1,2], evalParamFrame(parseExpression('floor{(1.5,2.5)}'), ev(0,0), 0))
-  assert(1/2, evalParamFrame(parseExpression('floor{0.6,to:1/2}'), ev(0,0), 0))
-  assert(-1.5, evalParamFrame(parseExpression('floor{-1.2,to:1/2}'), ev(0,0), 0))
+
+  assert(2, evalParamFrame(parseExpression('ceil{1.5}'), ev(0,0), 0))
+  assert(-1, evalParamFrame(parseExpression('ceil{-1.5}'), ev(0,0), 0))
+  assert(1/2, evalParamFrame(parseExpression('ceil{0.4,to:1/2}'), ev(0,0), 0))
+  assert(-1.5, evalParamFrame(parseExpression('ceil{-1.7,to:1/2}'), ev(0,0), 0))
+
+  assert(2, evalParamFrame(parseExpression('round{1.51}'), ev(0,0), 0))
+  assert(1, evalParamFrame(parseExpression('round{1.49}'), ev(0,0), 0))
+  assert(-1, evalParamFrame(parseExpression('round{-1.4}'), ev(0,0), 0))
+  assert(1/2, evalParamFrame(parseExpression('round{0.4,to:1/2}'), ev(0,0), 0))
+  assert(-1.5, evalParamFrame(parseExpression('round{-1.7,to:1/2}'), ev(0,0), 0))
 
   console.log('Maths tests complete')
   }
