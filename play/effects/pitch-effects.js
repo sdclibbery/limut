d9f2618d6dd1bcacc5b2466b2394c9cb8232434f
Stephen Clibbery <sdclibbery@laptop.local>
2022-12-23 16:42:57 +0000

    first attempt at glide

--- play/effects/pitch-effects.js
+++ play/effects/pitch-effects.js
@@ -23,13 +23,29 @@ define(function (require) {
     return v * vibdepth
   }
 
+  let glideSemis = (params, count, glide, glideBaseEvent) => {
+    if (!glideBaseEvent) { return 0 }
+    let lerp = (count - params.count) / glide
+    if (lerp > 1) { return 0 }
+    let baseFreq = glideBaseEvent.freq
+    let targetFreq = params.freq
+    let glideFreq = targetFreq*lerp + baseFreq*(1-lerp)
+    return 12 * Math.log2(glideFreq / targetFreq)
+  }
+
   return (audioParam, params) => {
+    let glide = evalMainParamEvent(params, 'glide', 0)
+    let glideBaseEvent
+    if (glide) {
+      let es = params._player.events.filter(e => e.voice === params.voice)
+      glideBaseEvent = es[es.length-1]
+    }
     // per event
     let detune = 0
     detune += evalMainParamEvent(params, 'addc', 0)
     setAudioParamValue(audioParam, detune*100, 'pitcheffects')
     // per frane
-    if (hasFrameParam(params, 'addc') || hasParam(params, 'vib')) {
+    if (hasFrameParam(params, 'addc') || hasParam(params, 'vib') || glideBaseEvent) {
       let vib = evalMainParamEvent(params, 'vib', 0)
       let vibdepth = evalSubParamEvent(params, 'vib', 'depth', 0.4)
       let vibdelay = evalSubParamEvent(params, 'vib', 'delay', 1/2)
@@ -37,6 +53,7 @@ define(function (require) {
         let detune = 0
         detune += evalMainPerFrame(params, 'addc', 0, count)
         detune += vibrato(vib, vibdepth, vibdelay, params.count, count)
+        detune += glideSemis(params, count, glide, glideBaseEvent)
         return detune*100 // Convert to cents for the detune audioParam
       })
       }
