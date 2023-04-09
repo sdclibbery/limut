a6a78cfcb907a1194901fcaf04f4ed6607bc15e9
Stephen Clibbery <sdclibbery@laptop.local>
2023-04-09 17:12:46 +0100

    predictable convolution reverb

--- play/effects/convolutionReverb.js
+++ play/effects/convolutionReverb.js
@@ -2,6 +2,15 @@
 define(function (require) {
   let system = require('play/system')
 
+  function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+  }
+
   return (duration, curve) => {
     let reverb = system.audio.createConvolver()
     var rate = system.audio.sampleRate
@@ -9,9 +18,10 @@ define(function (require) {
     var impulse = system.audio.createBuffer(2, length, rate)
     var impulseL = impulse.getChannelData(0)
     var impulseR = impulse.getChannelData(1)
+    let random = mulberry32(1) // Same random seed every time
     for (var i = 0; i < length; i++) {
-      impulseL[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, curve)
-      impulseR[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, curve)
+      impulseL[i] = (random() * 2 - 1) * Math.pow(1 - i / length, curve)
+      impulseR[i] = (random() * 2 - 1) * Math.pow(1 - i / length, curve)
     }
     reverb.buffer = impulse
     return reverb
