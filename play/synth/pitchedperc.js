271cd9216e746de36cbee46ce201b61296e600e4
Stephen Clibbery <sdclibbery@laptop.local>
2023-09-24 08:30:53 +0100

    initial bd909 work

--- play/synth/pitchedperc.js
+++ play/synth/pitchedperc.js
@@ -1,7 +1,7 @@
 'use strict';
 define(function (require) {
   let system = require('play/system');
-  let envelope = require('play/envelopes')
+  let destructor = require('play/destructor')
   let effects = require('play/effects/effects')
   let {fxMixChain} = require('play/effects/fxMixChain')
   let waveEffects = require('play/effects/wave-effects')
@@ -10,168 +10,144 @@ define(function (require) {
   let {getBuffer,getUrl} = require('play/samples')
   let {evalMainParamEvent,evalSubParamEvent} = require('play/eval-audio-params')
   let perFrameAmp = require('play/effects/perFrameAmp')
+  let timeToBeats = require('units').timeToBeats
 
   let clickBuffer
+  let clickTime = 0.001
   let getClick = () => {
     if (clickBuffer === undefined) {
       const sampleRate = system.audio.sampleRate
-      clickBuffer = system.audio.createBuffer(1, 0.1*sampleRate, sampleRate);
+      clickBuffer = system.audio.createBuffer(1, clickTime*sampleRate, sampleRate);
       let clickData = clickBuffer.getChannelData(0)
-      let impulseSamples = 0.001*sampleRate
       for (var i = 0; i < clickData.length; i++) {
-        if (i < impulseSamples)
-        {
-          clickData[i] = 1
-        } else {
-          let v = (i-impulseSamples) / clickData.length
-          let n = v*Math.random()*2
-          clickData[i] = 1-n
-        }
+        clickData[i] = 1
       }
     }
     return clickBuffer
   }
-  let click = (params, nodes) => {
+  let click = (params) => {
     let gain = evalMainParamEvent(params, 'click', 1)*0.07
-    if (gain <= 0.0001) { return }
-    let dur = evalSubParamEvent(params, 'click', 'dur', 1/5) * params.beat.duration
-    let cutoff = evalSubParamEvent(params, 'click', 'cutoff', 1500)
-    let q = evalSubParamEvent(params, 'click', 'q', 5)
+    if (gain <= 0.0001) { return undefined }
     let click = system.audio.createBufferSource()
-    nodes.push(click)
+    params._destructor.disconnect(click)
     click.buffer = getClick()
     click.start(params._time)
-    click.stop(params._time + dur)
-    let lpf = system.audio.createBiquadFilter()
-    nodes.push(lpf)
-    lpf.type = 'lowpass'
-    lpf.Q.value = q
-    lpf.frequency.value = cutoff
+    click.stop(params._time + click.buffer.duration)
+    params.endTime = Math.max(params.endTime, params._time+click.buffer.duration)
     let vca = system.audio.createGain()
-    nodes.push(vca)
-    vca.gain.cancelScheduledValues(0)
-    vca.gain.setValueAtTime(1, 0)
-    vca.gain.setValueAtTime(gain, params._time)
-    vca.gain.exponentialRampToValueAtTime(0.001, params._time + dur)
-    click.connect(lpf)
-    lpf.connect(vca)
+    params._destructor.disconnect(vca)
+    vca.gain.value = gain
+    click.connect(vca)
     return vca
   }
 
-  let curve = (init, final, power) => {
-    const steps = 19
-    const data = new Float32Array(steps)
-    for (let i=0; i<steps; i++) {
-      let lerp = Math.pow(i/(steps-1), 1/power)
-      data[i] = init*(1-lerp) + final*lerp
-    }
-    return data
-  }
-
-  let hit = (params, nodes) => {
+  let hit = (params) => {
     let gain = evalMainParamEvent(params, 'hit', 1)*0.25
-    if (gain <= 0.0001) { return }
+    if (gain <= 0.0001) { return undefined }
     let sample = evalSubParamEvent(params, 'hit', 'sample', '^')
     let sampleIdx = evalSubParamEvent(params, 'hit', 'index', 1)
     let rate = evalSubParamEvent(params, 'hit', 'rate', 3/2)
-    let cutoff = evalSubParamEvent(params, 'hit', 'cutoff', 250)
-    let q = evalSubParamEvent(params, 'hit', 'q', 1)
     let source = system.audio.createBufferSource()
-    nodes.push(source)
     source.buffer = getBuffer(getUrl(sample, sampleIdx))
+    if (!source.buffer) { return undefined }
+    params._destructor.disconnect(source)
     source.playbackRate.value = rate
-    let bufferDur =  (source.buffer ? source.buffer.duration : 0.1)
-    let lpf = system.audio.createBiquadFilter()
-    nodes.push(lpf)
-    lpf.type = 'lowpass'
-    lpf.Q.value = q
-    lpf.frequency.value = cutoff
     let vca = system.audio.createGain()
-    nodes.push(vca)
+    params._destructor.disconnect(vca)
     vca.gain.value = gain
-    source.connect(lpf)
-    lpf.connect(vca)
+    source.connect(vca)
     source.start(params._time)
-    source.stop(params._time+bufferDur)
+    source.stop(params._time+source.buffer.duration)
+    params.endTime = Math.max(params.endTime, params._time+source.buffer.duration)
     return vca
   }
 
-  let body = (params, nodes) => {
+  let body = (params) => {
     let gain = evalMainParamEvent(params, 'body', 1)*0.2
-    if (gain <= 0.0001) { return }
+    if (gain <= 0.0001) { return undefined }
+    let units = evalSubParamEvent(params, 'body', 'units', 'ms').toLowerCase()
+    let timeScale = timeToBeats(1, units, params)
+    let attack = evalSubParamEvent(params, 'body', 'att', 5)*timeScale
+    let decay = evalSubParamEvent(params, 'body', 'dec', 400)*timeScale
     let freq = evalSubParamEvent(params, 'body', 'freq', 55)
     let boost = evalSubParamEvent(params, 'body', 'boost', 150)
-    let pow = evalSubParamEvent(params, 'body', 'curve', 3)
+    let pitchdec = evalSubParamEvent(params, 'body', 'pitchdec', 50)*timeScale
     let wave = evalSubParamEvent(params, 'body', 'wave', 'sine')
-    let cutoff = evalSubParamEvent(params, 'body', 'cutoff', 2)
-    let q = evalSubParamEvent(params, 'body', 'q', 10)
     let vco = system.audio.createOscillator()
-    nodes.push(vco)
+    params._destructor.disconnect(vco)
     setWave(vco, wave)
-    vco.frequency.cancelScheduledValues(0)
-    vco.frequency.setValueAtTime(0, 0)
-    vco.frequency.setValueCurveAtTime(curve(freq+boost, freq, pow), params._time, params.endTime-params._time)
-    vco.start(params._time)
-    vco.stop(params.endTime)
-    let lpf = system.audio.createBiquadFilter()
-    nodes.push(lpf)
-    lpf.type = 'lowpass'
-    lpf.Q.value = q
-    lpf.frequency.cancelScheduledValues(0)
-    lpf.frequency.setValueAtTime(0, 0)
-    lpf.frequency.setValueCurveAtTime(curve((freq+boost)*cutoff, freq*cutoff, pow), params._time, params.endTime-params._time)
+    vco.frequency.setValueAtTime(0,0)
+    vco.frequency.setValueAtTime(freq+boost, params._time)
+    vco.frequency.exponentialRampToValueAtTime(freq, params._time + pitchdec)
     let vca = system.audio.createGain()
-    nodes.push(vca)
-    vca.gain.value = gain
-    vco.connect(lpf)
-    lpf.connect(vca)
+    params._destructor.disconnect(vca)
+    vca.gain.setValueAtTime(0,0)
+    vca.gain.setValueAtTime(0, params._time)
+    vca.gain.linearRampToValueAtTime(gain, params._time+attack)
+    vca.gain.exponentialRampToValueAtTime(0.00001, params._time + attack+decay)
+    vco.connect(vca)
+    vco.start(params._time)
+    vco.stop(params._time + attack+decay)
+    params.endTime = Math.max(params.endTime, params._time+attack+decay)
     return vca
   }
 
-  let rattle = (params, nodes) => {
+  let rattle = (params) => {
     let gain = evalMainParamEvent(params, 'rattle', 1)*0.4
-    if (gain <= 0.0001) { return }
+    if (gain <= 0.0001) { return undefined }
+    let units = evalSubParamEvent(params, 'rattle', 'units', 'ms').toLowerCase()
+    let timeScale = timeToBeats(1, units, params)
+    let attack = evalSubParamEvent(params, 'rattle', 'att', 0)*timeScale
+    let decay = evalSubParamEvent(params, 'rattle', 'dec', 30)*timeScale
     let rate = evalSubParamEvent(params, 'rattle', 'rate', 1)
     let freq = evalSubParamEvent(params, 'rattle', 'freq', 55)
     let boost = evalSubParamEvent(params, 'rattle', 'boost', 205)
-    let pow = evalSubParamEvent(params, 'rattle', 'curve', 8)
+    let pitchdec = evalSubParamEvent(params, 'rattle', 'pitchdec', 50)*timeScale
     let q = evalSubParamEvent(params, 'rattle', 'q', 18)
     let n = whiteNoise()
-    nodes.push(n)
-    n.start(params._time)
-    n.stop(params.endTime)
+    params._destructor.disconnect(n)
     n.playbackRate.value = rate
     let lpf = system.audio.createBiquadFilter()
-    nodes.push(lpf)
+    params._destructor.disconnect(lpf)
     lpf.type = 'lowpass'
     lpf.Q.value = q
-    lpf.frequency.cancelScheduledValues(0)
-    lpf.frequency.setValueAtTime(0, 0)
-    lpf.frequency.setValueCurveAtTime(curve(freq+boost, freq, pow), params._time, params.endTime-params._time)
+    lpf.frequency.setValueAtTime(0,0)
+    lpf.frequency.setValueAtTime(freq+boost, params._time)
+    lpf.frequency.exponentialRampToValueAtTime(freq, params._time + pitchdec)
     n.connect(lpf)
     let vca = system.audio.createGain()
-    nodes.push(vca)
-    vca.gain.value = gain
+    params._destructor.disconnect(vca)
+    vca.gain.setValueAtTime(0,0)
+    vca.gain.setValueAtTime(0, params._time)
+    vca.gain.linearRampToValueAtTime(gain, params._time+attack)
+    vca.gain.exponentialRampToValueAtTime(0.00001, params._time + attack+decay)
     lpf.connect(vca)
+    n.start(params._time)
+    n.stop(params._time + decay)
+    params.endTime = Math.max(params.endTime, params._time+decay)
     return vca
   }
 
   return (params) => {
-    let vca = envelope(params, 0.4, 'percussion')
-    fxMixChain(params, perFrameAmp(params, vca))
-    let mix = system.audio.createGain()
+    let vca = system.audio.createGain()
+    let gainbase = 0.5 * evalMainParamEvent(params, "loud", 1)
+    vca.gain.value = Math.max(0, gainbase * (typeof params.amp === 'number' ? params.amp : 1))
 
-    let nodes = []
+    params._destructor = destructor()
+    params.endTime = params._time
     let components = [
-      click(params, nodes),
-      hit(params, nodes),
-      body(params, nodes),
-      rattle(params, nodes),
+      click(params),
+      hit(params),
+      body(params),
+      rattle(params),
     ]
-    components.filter(c => c !== undefined)
-              .forEach(c => c.connect(mix))
-
+    let mix = system.audio.createGain()
+    components
+      .filter(c => c !== undefined)
+      .forEach(c => c.connect(mix))
+    setTimeout(() => params._destructor.destroy(), 100+(params.endTime - system.audio.currentTime)*1000)
+    fxMixChain(params, perFrameAmp(params, vca))
     waveEffects(params, effects(params, mix)).connect(vca)
-    params._destructor.disconnect(vca, mix, nodes)
+    params._destructor.disconnect(vca, mix)
   }
 });
