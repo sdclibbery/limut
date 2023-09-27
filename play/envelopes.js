6ee06acdbbb24fd468d3430895622c71a58cfa28
Stephen Clibbery <sdclibbery@laptop.local>
2023-09-27 08:00:57 +0100

    organ envelope

--- play/envelopes.js
+++ play/envelopes.js
@@ -26,6 +26,25 @@ define(function (require) {
     return vca
   }
 
+  let organEnvelope = (params, gainBase) => {
+    let dur = Math.max(0.01, evalMainParamEvent(params, 'sus', evalMainParamEvent(params, 'dur', 0.25)))
+    dur *= evalMainParamEvent(params, "long", 1)
+    let attack = evalMainParamEvent(params, 'att', 0.01) * params.beat.duration
+    let sustain = dur * params.beat.duration
+    let release = evalMainParamEvent(params, 'rel', 0.1) * params.beat.duration
+    let gain = Math.max(0.0001, gainBase * (typeof params.amp === 'number' ? params.amp : 1))
+    let vca = system.audio.createGain();
+    vca.gain.cancelScheduledValues(0)
+    vca.gain.setValueAtTime(0, 0)
+    vca.gain.setValueAtTime(0, params._time)
+    vca.gain.linearRampToValueAtTime(gain, params._time + attack)
+    vca.gain.linearRampToValueAtTime(gain, params._time + attack+sustain)
+    vca.gain.exponentialRampToValueAtTime(0.00001, params._time + attack+sustain+release)
+    params.endTime = params._time + attack+sustain+release
+    params._destructor.disconnect(vca)
+    return vca
+  }
+
   let simpleEnvelope = (params, gainBase) => {
     let dur = Math.max(0.01, evalMainParamEvent(params, 'sus', evalMainParamEvent(params, 'dur', 0.25)))
     dur *= evalMainParamEvent(params, "long", 1)
@@ -126,6 +145,7 @@ define(function (require) {
     let env
     switch (envelope) {
       case 'full': env = fullEnvelope(params, gainbase); break
+      case 'organ': env = organEnvelope(params, gainbase); break
       case 'simple': env = simpleEnvelope(params, gainbase); break
       case 'pad': env = padEnvelope(params, gainbase, 'cosine'); break
       case 'linpad': env = padEnvelope(params, gainbase, 'linear'); break
