5debf8c85f3c93aeeb13601409327698023b2381
Stephen Clibbery <sdclibbery@laptop.local>
2023-10-04 21:07:07 +0100

    list of keywords

--- pattern/parse-pattern.js
+++ pattern/parse-pattern.js
@@ -2,12 +2,15 @@
 define(function(require) {
   let eatWhitespace = require('expression/eat-whitespace')
   let literal = require('pattern/literal/literal.js')
-  let loop = require('pattern/operator/loop.js')
-  let concat = require('pattern/operator/concat.js')
+
+  let keywords = [
+    {keyword:'loop', r:parseInt, action:require('pattern/operator/loop.js')},
+    {keyword:'+', action:require('pattern/operator/concat.js')},
+  ]
 
   let isWhitespace = (char) => char === '' || char === ' ' || char === '\t'
   let keyword = (state, kw) => {
-    if (state.str.slice(state.idx, state.idx + kw.length) !== kw) { return false} // Keyword doesn't match
+    if (state.str.slice(state.idx, state.idx + kw.length).toLowerCase() !== kw) { return false} // Keyword doesn't match
     let char = state.str[state.idx + kw.length]
     if (!!char && !isWhitespace(char)) { return false } // Keyword not followed by whitespace or eof
     state.idx += kw.length+1
@@ -19,13 +22,14 @@ define(function(require) {
     eatWhitespace(state)
     if (state.str[state.idx] === undefined) { return undefined }
     let startIdx = state.idx
-    if (keyword(state, 'loop')) {
-      result = {keyword:'loop'}
+    for (let idx in keywords) {
+      let k = keywords[idx]
+      if (keyword(state, k.keyword)) {
+        result = Object.assign({}, k)
+        break
+      }
     }
-    else if (keyword(state, '+')) {
-      result = {keyword:'+'}
-    }
-    else {
+    if (!result) {
       result = literal(state)
     }
     result.src = state.str.slice(startIdx, state.idx)
@@ -48,25 +52,23 @@ define(function(require) {
     // Build result pattern expression
     let idx = 0
     let result = elements[idx++]
-    if (!elements[idx]) { // Nothing more
-    } else if (elements[idx].keyword === 'loop') {
-      idx++
-      let loopCountElement = elements[idx++]
-      if (!loopCountElement) { throw `Invalid pattern: missing loop count` }
-      let loopCount = parseInt(loopCountElement.src)
-      if (!loopCount) { throw `Invalid pattern: invalid loop count ${loopCountElement.src}` }
-      result = loop(result, loopCount)
-    }
-    else if (elements[idx].keyword === '+') {
-      idx++
-      let r = elements[idx++]
-      if (r.keyword !== undefined) { throw `Invalid pattern: invalid r argument to + : ${r.src}` }
-      result = concat(result, r)
+    while (!!elements[idx]) {
+      let element = elements[idx++]
+      if (element.keyword) {
+        let r = elements[idx++]
+        if (r === undefined) { throw `Invalid pattern: missing argument to operator ${element.keyword}` }
+        if (r.keyword !== undefined) { throw `Invalid pattern: invalid argument ${r.src} to operator ${element.keyword}` }
+        if (element.r) {
+          let src = r.src
+          r = element.r(src)
+          if (!r) { throw `Invalid pattern: invalid argument ${src} to operator ${element.keyword}` }
+        }
+        result = element.action(result, r)
+      }
     }
 
     // Finish up
     if (result.keyword) { throw `Invalid pattern: missing literal` }
-    if (elements[idx] !== undefined) { throw `Invalid pattern: extra pattern data ${elements[idx].src}` }
     result.playFromStart = playFromStart
     return result
   }
@@ -81,7 +83,7 @@ define(function(require) {
     let assertThrows = (expected, code) => {
       let got
       try {code()}
-      catch (e) { if ((''+e).includes(expected)) {got=true} else {console.trace(`Assertion failed.\n>>Expected throw: ${expected}\n>>Actual: ${e}`)} }
+      catch (e) { if ((''+e).includes(expected)) {got=true} else {console.trace(`Assertion failed.\n>>Expected throw: ${expected}\n>>Actual: ${e}`);} }
       finally { if (!got) console.trace(`Assertion failed.\n>>Expected throw: ${expected}\n>>Actual: none` ) }
     }
     let st = (str) => { return { str:str, idx:0 } }
@@ -89,15 +91,16 @@ define(function(require) {
     assert(true, keyword(st('loop 1'), 'loop'))
     assert(true, keyword(st('loop\t1'), 'loop'))
     assert(true, keyword(st('loop'), 'loop'))
+    assert(true, keyword(st('LOOP'), 'loop'))
+    assert(true, keyword(st('looP'), 'loop'))
     assert(false, keyword(st('loop 1'), 'loo'))
     assert(false, keyword(st('loop 1'), 'loopy'))
     assert(false, keyword(st('loop1'), 'loop'))
 
-    assertThrows('Invalid pattern: missing loop count', () => parsePattern(st('01 loop')))
-    assertThrows('Invalid pattern: invalid loop count foo', () => parsePattern(st('01 loop foo')))
-    assertThrows('Invalid pattern: extra pattern data foo', () => parsePattern(st('01 foo')))
-    assertThrows('Invalid pattern: extra pattern data foo', () => parsePattern(st('01 loop 1 foo')))
-    assertThrows('Invalid pattern: missing literal', () => parsePattern(st(' loop 1')))
+    assertThrows('Invalid pattern: missing argument to operator loop', () => parsePattern(st('01 loop')))
+    assertThrows('Invalid pattern: invalid argument foo to operator loop', () => parsePattern(st('01 loop foo')))
+    assertThrows('Invalid pattern: missing argument to operator +', () => parsePattern(st(' loop +')))
+    assertThrows('Invalid pattern: invalid argument loop to operator +', () => parsePattern(st('0 + loop')))
 
     console.log("Pattern parse tests complete")
   }
