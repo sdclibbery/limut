8dc2bb1acc149129fae113e64fe86612aa9d5aa2
Steve <sdclibbery@users.noreply.github.com>
2025-03-21 18:03:02 +0000

    split source nodes out

--- play/nodes/convolver.js
+++ play/nodes/convolver.js
@@ -1,8 +1,15 @@
 'use strict'
 define(function(require) {
+  let addVarFunction = require('predefined-vars').addVarFunction
   let system = require('play/system');
   let {evalMainParamEvent} = require('play/eval-audio-params')
   let {evalParamFrame} = require('player/eval-param')
+
+  let addNodeFunction = (k, v) => {
+    v.dontEvalArgs = true
+    v._thisVar = true // Do not evaluate node functions during expand-chords; should rename _thisVar really
+    addVarFunction(k, v)
+  }
 
   function mulberry32(a) {
     return function() {
@@ -78,6 +85,5 @@ define(function(require) {
     node.buffer = buffer
     return node
   }
-
-  return convolver
+  addNodeFunction('convolver', convolver)
 })
--- play/nodes/nodes.js
+++ play/nodes/nodes.js
@@ -4,16 +4,15 @@ define(function(require) {
   let system = require('play/system');
   let {evalMainParamEvent,evalMainParamFrame} = require('play/eval-audio-params')
   let {evalParamFrame,evalParamEvent} = require('player/eval-param')
-  let setWave = require('play/synth/waveforms/set-wave')
   var metronome = require('metronome')
   let {connect,isConnectable} = require('play/nodes/connect')
   let connectOp = require('expression/connectOp')
-  let {getBuffer} = require('play/samples')
-  let convolver = require('play/nodes/convolver')
+  require('play/nodes/convolver')
+  require('play/nodes/source')
 
   let addNodeFunction = (k, v) => {
     v.dontEvalArgs = true
-    v._thisVar = true // Do not evaluating node functions during expand-chords; should rename _thisVar really
+    v._thisVar = true // Do not evaluate node functions during expand-chords; should rename _thisVar really
     addVarFunction(k, v)
   }
 
@@ -109,68 +108,6 @@ define(function(require) {
   }
   addNodeFunction('idnode', idnode)
 
-  let osc = (args,e,b) => {
-    let node = system.audio.createOscillator()
-    let params = combineParams(args, e)
-    let value = evalParamEvent(params.value, e,b)
-    setWave(node, (typeof value === 'string') ? value :  'sawtooth')
-    let freq = 440
-    if (typeof value === 'number' && value !== 0) {
-      evalMainParamFrame(node.frequency, params, 'value', 440, 'hz')
-      freq = evalParamEvent(params['value'], e)
-    } else {
-      evalMainParamFrame(node.frequency, params, 'freq', 440, 'hz')
-      freq = evalParamEvent(params['freq'], e)
-    }
-    let phase = evalParamEvent(params['phase'], e)
-    let offset = 0
-    if (typeof freq === 'number' && typeof phase === 'number') { offset = phase / freq }
-    node.start(e._time + offset)
-    e._destructor.stop(node)
-    return node
-  }
-  addNodeFunction('osc', osc)
-
-  let sample = (args,e,b) => {
-    let node = system.audio.createBufferSource()
-    let params = combineParams(args, e)
-    let value = params.sample !== undefined ? params.sample : args.value
-    let startTime = 0
-    if (typeof value === 'string' || value === undefined) {
-      if (value === undefined) { value = 'sample/salamander/C4v8.mp3' }
-      node.buffer = getBuffer(value)
-      startTime = evalMainParamEvent(params, 'start', 0, 's')
-    } else {
-      const length = evalMainParamEvent(params, 'length', 0.2, 's')
-      const sampleRate = system.audio.sampleRate
-      let buffer = system.audio.createBuffer(1, length*sampleRate, sampleRate)
-      let data = buffer.getChannelData(0)
-      args.value.modifiers = args.value.modifiers || {}
-      for (var i = 0; i < data.length; i++) {
-        args.value.modifiers.value = i / data.length
-        let y = evalParamFrame(args.value,e,b, {doNotMemoise:true}) // It will memoise the same result across all x if allowed to
-        data[i] = y
-      }
-      node.buffer = buffer
-    }
-    evalMainParamFrame(node.playbackRate, params, 'rate', 1)
-    node.loop = params.loop === undefined || params.loop === true ? true : false // Default to looping if not set
-    node.start(e._time, startTime)
-    e._destructor.stop(node)
-    return node
-  }
-  addNodeFunction('sample', sample)
-
-  let constNode = (args,e,b) => {
-    let node = system.audio.createConstantSource()
-    let params = combineParams(args, e)
-    evalMainParamFrame(node.offset, params, 'value', 1)
-    node.start(e._time)
-    e._destructor.stop(node)
-    return node
-  }
-  addNodeFunction('const', constNode)
-
   let gain = (args,e,b) => {
     let node = system.audio.createGain()
     let params = combineParams(args, e)
@@ -207,8 +144,6 @@ define(function(require) {
     return node
   }
   addNodeFunction('shaper', shaper)
-
-  addNodeFunction('convolver', convolver)
 
   let delay = (args,e,b) => {
     let maxDelay = evalMainParamEvent(args, 'max', 1, 'b') * metronome.beatDuration()
