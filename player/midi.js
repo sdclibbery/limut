9bcb4d2a417b74e3018b257940d5c56de76cef54
Steve <sdclibbery@users.noreply.github.com>
2025-07-26 00:07:03 +0100

    midi percussion mapping

--- player/midi.js
+++ player/midi.js
@@ -4,21 +4,46 @@ define(function(require) {
   let metronome = require('metronome')
   let {combineOverrides,applyOverrides} = require('player/override-params')
 
+  let applyMapping = (event, note, mapping) => {
+    if (mapping === 'perc') {
+      event.value = {
+        35:'x', 36:'X', 37:'t', 38:'o', 39:'H', 40:'u', 41:'m', 42:'-', 43:'M',
+        46:'o', 49:'#', 54:'S', 56:'T', 
+      }[note] || '-'
+    } else if (mapping === 'abs') {
+      event.addc = note-60 // Not actually absolute; either need a new param or rescale to root/scale/oct
+    } else {
+      event.sharp = note-60 // Doesn't play well with add=() chords, will have to do a proper inverse mapping?
+    }
+  }
+
   let midiPlayer = (patternStr, params, player, baseParams) => {
       // parse pattern string to get port/channel
       let patternArgs = patternStr.split(/\s+/)
+      let mapping = 'rel'
       let port, channel
+      patternArgs = patternArgs
+        .map(arg => arg.trim())
+        .filter(arg => arg !== '')
+        .map(arg => !isNaN(parseInt(arg,10)) ? parseInt(arg,10) : arg)
+      if (typeof patternArgs[0] === 'string')  {
+        mapping = patternArgs[0]
+        patternArgs = patternArgs.slice(1)
+      }
       if (patternArgs.length === 1) {
         port = 0
         channel = parseInt(patternArgs[0], 10) || 0
       } else if (patternArgs.length === 2) {
         port = parseInt(patternArgs[0], 10) || 0
         channel = parseInt(patternArgs[1], 10) || 0
+      } else {
+        port = 0
+        channel = 0
       }
       // Listen to given midi port/channel, create appropriate event and call player.play()
       midi.listen(port, channel, player.id, (note, velocity) => {
         let event = {
-          value: note-60, // if channel 9, convert to percussion samples, else midi to scale
+          value: 0,
           dur: 1,
           vel: velocity,
           _time: metronome.timeNow(),
@@ -26,6 +51,7 @@ define(function(require) {
           idx: metronome.lastBeat().count,
           beat: metronome.lastBeat(),
         }
+        applyMapping(event, note, mapping)
         event.sound = event.value
         event = combineOverrides(event, baseParams)
         event = applyOverrides(event, params)
