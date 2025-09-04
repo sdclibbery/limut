a3aa0a518a741131a09d9acfb006aa2bf146e766
Steve <sdclibbery@users.noreply.github.com>
2025-09-04 20:41:16 +0100

    fallback to debug serial port

--- electron-main.js
+++ electron-main.js
@@ -9,14 +9,20 @@ app.on('ready', () => {
     console.log('Available serial ports:', portList)
     if (portList.length > 0) {
       let portNameTarget = 'cu.usbserial'//'cu.debug-console'
-      const selectedPort = portList.filter(p => p.portName.toLowerCase().includes(portNameTarget))[0] // First port that includes the target string
+      let selectedPort = portList.filter(p => p.portName.toLowerCase().includes(portNameTarget))[0] // First port that includes the target string
       if (!!selectedPort) {
         console.log(`DMX using serial port: ${selectedPort.portName} ${selectedPort.portId}`)
         callback(selectedPort.portId)
         return
       }
+      selectedPort = portList.filter(p => p.portName.toLowerCase().includes('debug'))[0] // Fallback to debug console to allow dev work without a physical dmx controller attached
+      if (!!selectedPort) {
+        console.log(`No DMX serial ports found, falling back to ${selectedPort.portName} ${selectedPort.portId}.`)
+        callback(selectedPort.portId)
+        return
+      }
     }
-    console.log('No serial ports found.')
+    console.log('No DMX serial ports found.')
     callback('') // No port selected
   })
   mainWindow.loadURL(`file://${__dirname}/index.html`)
