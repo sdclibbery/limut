1a78a8febcec3514f71c47187a5a7836ac4fa0b6
Steve <sdclibbery@users.noreply.github.com>
2025-12-16 08:47:19 +0000

    refactor gamepad named axes

--- functions/gamepad.js
+++ functions/gamepad.js
@@ -3,6 +3,33 @@ define(function(require) {
   let consoleOut = require('console')
   let addVar = require('predefined-vars').add
 
+  let namedAxes = {
+    lt : { button: 6 }, // left trigger
+    rt : { button: 7 }, // right trigger
+    lsh : { axis: 0 }, // left stick horizontal
+    lsv : { axis: 1 }, // left stick vertical
+    rsh : { axis: 2 }, // right stick horizontal
+    rsv : { axis: 3 }, // right stick vertical
+    lsl : { axis: 0, range: 'neg' }, // left stick left
+    lsr : { axis: 0, range: 'pos' }, // left stick right
+    lsd : { axis: 1, range: 'neg' }, // left stick down
+    lsu : { axis: 1, range: 'pos' }, // left stick up
+    lsud : { axis: 1, range: 'both' }, // left stick up or down
+    lsdu : { axis: 1, range: 'both' }, // left stick up or down
+    lslr : { axis: 0, range: 'both' }, // left stick left or right
+    lsrl : { axis: 0, range: 'both' }, // left stick left or right
+    lsx : { axis: 0, range: 'radial' }, // left stick radial
+    rsl : { axis: 2, range: 'neg' }, // right stick left
+    rsr : { axis: 2, range: 'pos' }, // right stick right
+    rsd : { axis: 3, range: 'neg' }, // right stick down
+    rsu : { axis: 3, range: 'pos' }, // right stick up
+    rsud : { axis: 3, range: 'both' }, // right stick up or down
+    rsdu : { axis: 3, range: 'both' }, // right stick up or down
+    rslr : { axis: 2, range: 'both' }, // right stick left or right
+    rsrl : { axis: 2, range: 'both' }, // right stick left or right
+    rsx : { axis: 2, range: 'radial' }, // right stick radial
+  }
+
   let blankArgs = {}
   let newGamepad = (args, context) => {
     args = args || blankArgs
@@ -14,31 +41,11 @@ define(function(require) {
     if (typeof args.value === 'string') {
       requireStandardMapping = true
       let str = args.value.toLowerCase().trim()
-      if (str === 'lt') { buttonNumber = 6 } // left trigger
-      else if (str === 'rt') { buttonNumber = 7 } // right trigger
-      else if (str === 'lsh') { axisNumber = 0 } // left stick horizontal
-      else if (str === 'lsv') { axisNumber = 1 } // left stick vertical
-      else if (str === 'rsh') { axisNumber = 2 } // right stick horizontal
-      else if (str === 'rsv') { axisNumber = 3 } // right stick vertical
-      else if (str === 'lsl') { axisNumber = 0; axisRange = 'neg' } // left stick left
-      else if (str === 'lsr') { axisNumber = 0; axisRange = 'pos' } // left stick right
-      else if (str === 'lsd') { axisNumber = 1; axisRange = 'neg' } // left stick down
-      else if (str === 'lsu') { axisNumber = 1; axisRange = 'pos' } // left stick up
-      else if (str === 'lsud') { axisNumber = 1; axisRange = 'both' } // left stick up or down
-      else if (str === 'lsdu') { axisNumber = 1; axisRange = 'both' } // left stick up or down
-      else if (str === 'lslr') { axisNumber = 0; axisRange = 'both' } // left stick left or right
-      else if (str === 'lsrl') { axisNumber = 0; axisRange = 'both' } // left stick left or right
-      else if (str === 'lsx') { axisNumber = 0; axisRange = 'radial' } // left stick radial
-      else if (str === 'rsl') { axisNumber = 2; axisRange = 'neg' } // right stick left
-      else if (str === 'rsr') { axisNumber = 2; axisRange = 'pos' } // right stick right
-      else if (str === 'rsd') { axisNumber = 3; axisRange = 'neg' } // right stick down
-      else if (str === 'rsu') { axisNumber = 3; axisRange = 'pos' } // right stick up
-      else if (str === 'rsud') { axisNumber = 3; axisRange = 'both' } // right stick up or down
-      else if (str === 'rsdu') { axisNumber = 3; axisRange = 'both' } // right stick up or down
-      else if (str === 'rslr') { axisNumber = 2; axisRange = 'both' } // right stick left or right
-      else if (str === 'rsrl') { axisNumber = 2; axisRange = 'both' } // right stick left or right
-      else if (str === 'rsx') { axisNumber = 2; axisRange = 'radial' } // right stick radial
-    } else if (axisNumber === undefined) { axisNumber = args.value }
+      let axis = namedAxes[str]
+      if (axis && axis.axis !== undefined) { axisNumber = axis.axis } else { axisNumber = args.value }
+      if (axis && axis.button !== undefined) { axisButton = axis.button }
+      if (axis && axis.range !== undefined) { axisRange = axis.range }
+    }
     let lastStr
     let gamepadValue = () => {
       if (padNumber === undefined && axisNumber === undefined && buttonNumber === undefined) { // log gamepad info if no params
@@ -77,30 +84,9 @@ define(function(require) {
   newGamepad._name = 'gamepad'
 
   // Handy shortcuts so you can just write `gp.lsr` etc
-  newGamepad.lt = newGamepad.bind(null, {value: 'lt'})
-  newGamepad.rt = newGamepad.bind(null, {value: 'rt'})
-  newGamepad.lsh = newGamepad.bind(null, {value: 'lsh'})
-  newGamepad.lsv = newGamepad.bind(null, {value: 'lsv'})
-  newGamepad.rsh = newGamepad.bind(null, {value: 'rsh'})
-  newGamepad.rsv = newGamepad.bind(null, {value: 'rsv'})
-  newGamepad.lsl = newGamepad.bind(null, {value: 'lsl'})
-  newGamepad.lsr = newGamepad.bind(null, {value: 'lsr'})
-  newGamepad.lsu = newGamepad.bind(null, {value: 'lsu'})
-  newGamepad.lsd = newGamepad.bind(null, {value: 'lsd'})
-  newGamepad.lsud = newGamepad.bind(null, {value: 'lsud'})
-  newGamepad.lsdu = newGamepad.bind(null, {value: 'lsdu'})
-  newGamepad.lslr = newGamepad.bind(null, {value: 'lslr'})
-  newGamepad.lsrl = newGamepad.bind(null, {value: 'lsrl'})
-  newGamepad.lsx = newGamepad.bind(null, {value: 'lsx'})
-  newGamepad.rsl = newGamepad.bind(null, {value: 'rsl'})
-  newGamepad.rsr = newGamepad.bind(null, {value: 'rsr'})
-  newGamepad.rsu = newGamepad.bind(null, {value: 'rsu'})
-  newGamepad.rsd = newGamepad.bind(null, {value: 'rsd'})
-  newGamepad.rsud = newGamepad.bind(null, {value: 'rsud'})
-  newGamepad.rsdu = newGamepad.bind(null, {value: 'rsdu'})
-  newGamepad.rslr = newGamepad.bind(null, {value: 'rslr'})
-  newGamepad.rsrl = newGamepad.bind(null, {value: 'rsrl'})
-  newGamepad.rsx = newGamepad.bind(null, {value: 'rsx'})
+  for (let key in namedAxes) {
+    newGamepad[key] = newGamepad.bind(null, {value: key})
+  }
 
   addVar('gamepad', newGamepad)
   addVar('gp', newGamepad) // Alias for ease of access
