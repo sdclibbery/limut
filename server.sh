2f3378b11f1df8399279024ac3a2ea3feccd73df
Steve <sdclibbery@users.noreply.github.com>
2026-09-25 23:51:49 +0100

    better server

--- server.sh
+++ server.sh
@@ -1,1 +1,2 @@
-python3 -m http.server 8000 &
+# http.server's default listen backlog of 5 drops connections when requirejs fetches modules in a burst
+python3 -c "import http.server as h; h.ThreadingHTTPServer.request_queue_size=256; h.test(HandlerClass=h.SimpleHTTPRequestHandler, ServerClass=h.ThreadingHTTPServer, port=8000)" &
